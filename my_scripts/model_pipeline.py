6c42c4c0535fefb6e63ec202d9d2a35c6ea0a370
LorenzoVaraschin <lorenzovaraschin201213@gmail.com>
2023-06-21 17:02:46 +0000

    Added new scripts

---
+++ my_scripts/model_pipeline.py
@@ -0,0 +1,89 @@
+"""
+Creates the model's training and testing pipeline
+"""
+import torch
+import wandb
+
+from make_dataloader import make_dataloaders
+from make_models import RegressionResNet18
+from train_and_test_pipeline import train_model, test_epoch
+from torch.optim.lr_scheduler import ReduceLROnPlateau, StepLR
+
+
+def model_pipeline(project, hyperparameters, df_train, df_test, df_val):
+  """
+  Creates the model and dataloaders and trains/tests the model. Log the results in wandb.
+
+  Args:
+  Project: Name of the project to be logged in wandb. 
+  hyperparameters: Hyperparamaters of the model and config of the wandb log.
+
+  Returns:
+  Trained model.
+  """
+
+  with wandb.init(project=project, config=hyperparameters):
+    config = wandb.config
+
+    #Create the dataloaders
+    train_dataloader, test_dataloader, val_dataloader = make_dataloaders(df_train=df_train, df_test=df_test, df_val=df_val, config=hyperparameters)
+
+    #Create the model
+    model = RegressionResNet18()
+
+    #Create the loss function
+    assert hyperparameters["loss_fn"] in ["mse_loss"], "Invalid loss function"
+    if hyperparameters["loss_fn"] == "mse_loss":
+      loss_fn = torch.nn.MSELoss()
+
+    assert hyperparameters["optimizer"] in ["adam"], "Invalid optimizer"
+    if hyperparameters["optimizer"] == "adam":
+      optimizer = torch.optim.Adam(
+        params=model.parameters(),
+        lr=hyperparameters["learning_rate"]
+      )
+
+    #Create the scheduler
+    assert hyperparameters["scheduler"] in ["step_lr", "reduce_lr_on_plateau"], "Invalid Scheduler"
+    if hyperparameters["scheduler"] == "step_lr":
+      scheduler = StepLR(optimizer, step_size=hyperparameters["scheduler_step_size"], gamma=hyperparameters["scheduler_factor"])
+    elif hyperparameters["scheduler"] == "reduce_lr_on_plateau":
+      scheduler = ReduceLROnPlateau(optimizer, 'min', factor=hyperparameters["scheduler_factor"], patience=hyperparameters["scheduler_patience"])
+
+    #Train the model
+    train_model(
+      model=model,
+      train_dataloader=train_dataloader,
+      val_dataloader=val_dataloader,
+      optimizer=optimizer,
+      scheduler=scheduler,
+      scheduler_name=hyperparameters["scheduler"],
+      epochs=hyperparameters["epochs"],
+      t_cls=hyperparameters["t_cls"],
+      target=hyperparameters["target"],
+      device=hyperparameters["device"],
+      num_extra_features=hyperparameters["num_extra_features"]
+    )
+
+    #Test the model
+    test_loss, test_rmse, test_mae = test_epoch(
+      model=model,
+      dataloader=test_dataloader,
+      loss_fn=loss_fn,
+      target=hyperparameters["target"],
+      device=hyperparameters["device"],
+      dataset="test",
+      t_cls=hyperparameters["t_cls"],
+      num_extra_features=hyperparameters["num_extra_features"]
+    )
+
+    wandb.log({"test_rmse": test_rmse, "test_mae": test_mae})
+
+    # Save the model in the exchangeable ONNX format
+    train_features, train_labels = next(iter(train_dataloader))
+    torch.onnx.export(model, train_features, "model.onnx")
+    wandb.save("model.onnx")
+
+    return model
+    
+
---
+++ my_scripts/train_and_test_pipeline.py
@@ -0,0 +1,187 @@
+"""
+Contains the train_epoch and test_epoch functions
+"""
+
+import torch
+import numpy as np
+import wandb
+
+from tqdm import tqdm
+from sklearn.metrics import mean_squared_error as sklearn_mse
+from sklearn.metrics import mean_absolute_error as sklearn_mae
+
+def train_epoch(model, dataloader, loss_fn, optimizer, epoch, target, device, t_cls=0.05, num_extra_features=0):
+  """
+  Trains the PyTorch model for one epoch. 
+  Forward pass -> Optimizer zero grad -> Backpropagation -> Optimizer step
+
+  Args:
+  model: Pytorch model that will be trained.
+  dataloader: PyTorch train dataloader that will be used to train the model.
+  loss_fn: PyTorch loss function to be optimized by the optimizer.
+  optimizer: PyTorch optimizer algorithm that will be used to optimize the loss function.
+  epoch: Epoch that is currently being trained. Used for printing purposes.
+  target: The label of each input, such as future_ghi, future_kt_solis and so on...
+  t_cls: Also not necessary yet, will be used to compute ramp metric.
+  device: Device that will do the computations (e.g. "cuda" or "cpu").
+  num_extra_features: If the model takes the image plus some extra features as it's input, num_extra_features is how many extra features there are.
+
+  Returns:
+  train_loss and train_rmse 
+  """
+
+  model.train()
+  train_loss, train_rmse = 0, 0
+  with tqdm(dataloader, unit="batch") as tepoch:
+    #X contains all the image data and aux contains all the scalar data, as it was when the PyTorch Dataset was created.
+    for X, aux in tepoch: 
+      #Printing purposes
+      tepoch.set_description(f"Epoch{epoch}") 
+
+      #time_stamps, ghi_cs, future_ghi_cs, future_ghi are all auxiliary tensors and are not used for training
+      time_stamps = aux[:, 0].detach()
+      ghi_cs = aux[:, 1].detach()
+      future_ghi_cs = aux[:, 2].detach()
+      future_ghi = aux[:, 3].detach()
+
+      #y contains the label its first column and all the extra features in the subsequent columns
+      y = aux[:, 4:].float()
+      X, y = X.to(device, non_blocking=True), y.to(device, non_blocking=True)
+
+      #Forward pass
+      if num_extra_features > 0:
+        y_pred = model(X, y[:, 1:])
+      else:
+        y_pred = model(X)
+
+      #Calculate the loss
+      loss = loss_fn(y_pred.squeeze(), y[:, 0])
+
+      #Accumulate the loss, if the target is the future kt and not ghi rescale it back to ghi in order to print the loss curves 
+      if "ghi" in target:
+        train_loss += loss.item()
+      else:
+        train_loss += sklearn_mse(future_ghi, y_pred.squeeze().detach().to("cpu") * future_ghi_cs).item()
+    
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      
+      #Printing purposes
+      tepoch.set_postfix(loss=train_loss)
+
+  train_loss = train_loss / len(dataloader)
+  train_rmse = np.sqrt(train_loss) #Loss = MSE
+  return train_loss, train_rmse
+
+def test_epoch(model, dataloader, loss_fn, target, device, dataset="test", t_cls=0.05, num_extra_features=0):
+  """
+  Test the PyTorch model for one epoch.
+
+  Args:
+  model: Pytorch model that will be evaluated.
+  dataloader: PyTorch test dataloader that will be used to evaluate the model.
+  loss_fn: Same PyTorch loss function that was used during training.
+  test_slopes_true: Not necessary as of yet. Will be used to compute ramp metric.
+  year: Also not necessary yet, will be used to compute ramp metric.
+  target: The label of each input, such as future_ghi, future_kt_solis and so on...
+  dataset: Validation or test dataset. Default: test.
+  t_cls: Also not necessary yet, will be used to compute ramp metric.
+  device: Device that will do the computations (e.g. "cuda" or "cpu").
+  num_extra_features: If the model takes the image plus some extra features as it's input, num_extra_features is how many extra features there are.
+
+  Returns:
+  test_loss, test_rmse and test_mae for the test dataset. val_loss, val_rmse for the val dataset. 
+  """
+  model.eval()
+  test_loss, test_rmse, test_mae = 0, 0, 0
+  with torch.inference_mode():
+    #X contains all the image data and aux contains all the scalar data, as it was when the PyTorch Dataset was created.
+    for X, aux in dataloader:
+
+      #time_stamps, ghi_cs, future_ghi_cs, future_ghi are all auxiliary tensors and are not used for training
+      time_stamps = aux[:, 0].detach()
+      ghi_cs = aux[:, 1].detach()
+      future_ghi_cs = aux[:, 2].detach()
+      future_ghi = aux[:, 3].detach()
+
+      #y contains the label its first column and all the extra features in the subsequent columns
+      y = aux[:, 4:].float()
+      X, y = X.to(device), y.to(device)
+
+      #Forward pass
+      if num_extra_features > 0:
+        test_pred = model(X, y[:, 1:])
+      else:
+        test_pred = model(X)
+
+      #Calculate the loss
+      loss = loss_fn(test_pred.squeeze(), y[:, 0])
+
+      #Accumulate the loss, if the target is the future kt and not ghi rescale it back to ghi in order to print the loss curves
+      if "ghi" in target:
+        test_loss += loss.item()
+        if dataset == "test":
+          test_mae += sklearn_mae(test_pred.squeeze(), y[:, 0]).item()
+      else:
+        test_loss += sklearn_mse(future_ghi, test_pred.squeeze().detach().to("cpu") * future_ghi_cs).item()
+        if dataset == "test":
+          test_mae += sklearn_mae(future_ghi, test_pred.squeeze().detach().to("cpu") * future_ghi_cs).item()
+
+  test_loss = test_loss / len(dataloader)
+  test_rmse = np.sqrt(test_loss)
+
+  if dataset == "test":
+    test_mae = test_mae/len(dataloader)
+    return test_loss, test_rmse, test_mae
+  else:
+    return test_loss, test_rmse
+
+def train_model(model, train_dataloader, val_dataloader, optimizer, loss_fn, scheduler, scheduler_name, epochs, t_cls, target, device, num_extra_features=0):
+  """
+  Trains and validates the model for the number of epochs specified.
+
+  Args:
+  model: Pytorch model that will be trained.
+  train_dataloader: PyTorch train dataloader that will be used to train the model.
+  val_dataloader: PyTorch validation dataloader that will be used to validate the model.
+  optimizer: PyTorch optimizer algorithm that will be used to optimize the loss function.
+  loss_fn: PyTorch loss function to be optimized by the optimizer.
+  scheduler: PyTorch learning rate scheduler.
+  scheduler_name: Name of the PyTorch learning rate scheduler.
+  epochs: Number of epochs to train the model.
+  t_cls: Not necessary yet, will be used to compute ramp metric.
+  target: The label of each input, such as future_ghi, future_kt_solis and so on...
+  device: Device that will do the computations (e.g. "cuda" or "cpu").
+  num_extra_features: If the model takes the image plus some extra features as it's input, num_extra_features is how many extra features there are.
+  """
+  wandb.watch(model, loss_fn, log="all", log_freq=100)
+  for epoch in range(epochs):
+    train_loss, train_rmse = train_epoch(
+      model=model,
+      dataloader=train_dataloader,
+      loss_fn=loss_fn,
+      optimizer=optimizer,
+      epoch=epoch,
+      target=target,
+      device=device,
+      t_cls=t_cls,
+      num_extra_features=num_extra_features
+    )
+
+    val_loss, val_rmse = test_epoch(
+      model=model,
+      dataloade=val_dataloader,
+      loss_fn=loss_fn,
+      target=target,
+      device=device,
+      dataset="val",
+      t_cls=t_cls,
+      num_extra_features=num_extra_features
+    )
+    wandb.log({"train_rmse": train_rmse, "val_rmse": val_rmse, "lr": optimizer.param_groups[0]['lr']})
+    print(f"Epoch: {epoch} | Train loss: {train_loss:.4f} | Train RMSE: {train_rmse:.4f} | Val loss: {val_loss:.4f} | Val RMSE: {val_rmse:.4f} | LR: {optimizer.param_groups[0]['lr']}")
+    if "reduce_lr_on_plateau" in scheduler_name:
+      scheduler.step(val_loss)
+    else:
+      scheduler.step()
