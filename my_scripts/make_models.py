131320dac519f6ffe6fad14ed9ae00a6b2d37f6e
LorenzoVaraschin <lorenzovaraschin201213@gmail.com>
2023-10-17 22:18:20 +0000

    Added new model

--- my_scripts/make_models.py
+++ my_scripts/make_models.py
@@ -66,20 +66,21 @@ class RegressionResNet18EmbedTransform(nn.Module):
           nn.Dropout(p=dropout),
           nn.Linear(in_features=512,
                     out_features=hidden_units),
-          nn.ReLU(),
-          nn.Linear(in_features=hidden_units,
-                    out_features=1)
+          nn.ReLU()
         )
+        self.linear = nn.Linear(in_features=hidden_units,
+                                    out_features=1)
       else:
         self.resnet.fc = nn.Sequential(
           nn.Linear(in_features=512,
                     out_features=hidden_units),
-          nn.ReLU(),
-          nn.Linear(in_features=hidden_units,
-                    out_features=1)
+          nn.ReLU()
         )
+        self.linear = nn.Linear(in_features=hidden_units,
+                                    out_features=1)
   def forward(self, x):
     x = self.resnet(x)
+    x = self.linear(x)
     return x    
 
 class RegressionResNet50(nn.Module):
